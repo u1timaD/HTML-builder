35301342d17165b787a0154de9d524465cec21bf
Dmitry <free-doom46@mail.ru>
2023-05-03 23:07:21 +0300

    fix: change process

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,16 +1,15 @@
-const { stdin, stdout, exit } = process;
-const fs = require('fs');
-const path = require('path');
+const { stdout, exit } = process;
+import { readFile } from 'fs';
+import { join } from 'path';
 
 const dir = process.argv[1];
-const asd = fs.readFile(path.join(__dirname, 'text.txt'),
+const asd = readFile(join(__dirname, 'text.txt'),
 'utf-8',
 (err, data) => {
 	if (err) throw err;
 	stdout.write(data);
 	exit()
 	}
-)
-
+);
 
 
