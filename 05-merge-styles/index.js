be4e3411c5719a12621343db5ac7274780d11647
Dmitry <free-doom46@mail.ru>
2023-05-06 16:13:49 +0300

    feat: finish task 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,4 +2,35 @@ const fs = require('fs');
 const path = require('path');
 
 const projectDist = path.join(__dirname, 'project-dist');
-console.log(projectDist)
+const stylesDist = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname, 'bundle.css');
+
+
+fs.writeFile(path.join(projectDist, 'bundle.css'), '', (err) => {
+	if (err) throw err;
+
+fs.readdir(stylesDist, (err, files) => {
+	if(err) {
+		console.log('Ошибочка');
+	}
+
+	for(const file of files) {
+		const reg = /.css/g
+
+		fs.stat(path.join(stylesDist, file), (err, status) => {
+			if (err) throw err;
+			
+			if(status.isFile() && reg.test(file)) {
+				fs.readFile(path.join(stylesDist, file), 'utf-8', (err, data) => {
+					if (err) throw err;
+				fs.appendFile(path.join(projectDist, 'bundle.css'), data, (err) => {
+					if (err) throw err;
+				});
+			})
+	
+			}
+		})
+	}
+})
+
+})
